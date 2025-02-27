b51a9a0df79890496a81c1855b24512cbda06a82
sraya5 <sites.srayaa@gmail.com>
2025-02-28 00:48:51 +0200

    Update topic.js

--- references_files/js/topic.js
+++ references_files/js/topic.js
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     child._is_open = !child._is_open
                     if (child._is_open) {
                         parent.style.border = "1px solid";
-                        parent.style.padding = "1em";
+                        parent.style.padding = "0.5em";
                         tails.forEach(sibling => {
                             if (sibling.style) {
                                 sibling.style.display = sibling._display
